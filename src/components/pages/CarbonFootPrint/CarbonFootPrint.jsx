5962fd5cce3f9f08543f46e803c5cfa9653aade6
Sam Cladson <43487858+samcladson@users.noreply.github.com>
2021-12-17 18:23:32 +0530

    bug fixes

--- src/components/pages/CarbonFootPrint/CarbonFootPrint.jsx
+++ src/components/pages/CarbonFootPrint/CarbonFootPrint.jsx
@@ -2,22 +2,45 @@ import "./CarbonFootPrint.css";
 
 import ErrorModal from "../../ErrorModal/ErrorModal";
 import StepsSidebar from "../../StepsSidebar/StepsSidebar";
-import { useDispatch } from "react-redux";
-import {
-  selectModalVisisbility,
-  setModalVisibility,
-} from "../../../redux/reducers/appSlice";
 import { useSelector } from "react-redux";
 import InputModal from "../../inputModal/InputModal";
 import CarbonCost from "../../CarbonCost/CarbonCost";
 
 const CarbonFootPrint = () => {
-  const dispatch = useDispatch();
+  const [current, setCurrent] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [error, setError] = useState("");
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const isModalVisible = useSelector(selectModalVisisbility);
-  const showHideModal = () => {
-    dispatch(setModalVisibility(!isModalVisible));
+  const showHideModal = () => setIsModalVisible(!isModalVisible);
+  const updateWidth = () => {
+    setWidth(window.innerWidth);
   };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  });
+  const next = () => {
+    setError("");
+    if (current >= 0 && current <= 3) setCurrent(current + 1);
+  };
+  const prev = () => {
+    setError("");
+    if (current >= 1) setCurrent(current - 1);
+  };
+
+  // const handleError = () => {
+  //   setCurrent(0);
+  //   setError("error");
+  //   showHideModal();
+  // };
+
+  const handleStepChange = (e) => {
+    console.log(e);
+    setCurrent(e);
+  };
+  
   return (
     <>
       <ErrorModal
