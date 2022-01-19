39fb3cbd0bcf5dfb50c6f3a3a9f78d49255cdebb
Ashish Sah <ashishsah173@gmail.com>
2022-01-19 13:31:30 +0530

    Firebase change

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,15 @@
-// import { intializeApp } from "firebase/app";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCUkHKquqH4Y_oynHTdtECTFm97xTNmy8Y",
-//   authDomain: "myenergy-ai.firebaseapp.com",
-//   projectId: "myenergy-ai",
-//   storageBucket: "myenergy-ai.appspot.com",
-//   messagingSenderId: "523493172739",
-//   appId: "1:523493172739:web:d19efcc9b4001ab1209036",
-//   measurementId: "G-CR55F2SY1W",
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCUkHKquqH4Y_oynHTdtECTFm97xTNmy8Y",
+  authDomain: "myenergy-ai.firebaseapp.com",
+  projectId: "myenergy-ai",
+  storageBucket: "myenergy-ai.appspot.com",
+  messagingSenderId: "523493172739",
+  appId: "1:523493172739:web:d19efcc9b4001ab1209036",
+  measurementId: "G-CR55F2SY1W",
+};
 
-// const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
